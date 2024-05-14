6f3182b8204236b4a0c51ec9248d54149789e0e5
Eleonora <eleonora.avaleva@gmail.com>
2024-05-14 14:06:44 +0200

    adds skeleton

---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+    <div class="skeleton-loader">
+        <div class="skeleton-card"></div>
+        <div class="skeleton-card"></div>
+    </div>
+</template>
+
+<style scoped>
+.skeleton-loader {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.skeleton-card {
+    padding: 20px;
+    width: 250px;
+    height: 50px;
+    background-color: #f4f4f4;
+    margin: 18px;
+    border-radius: 10px;
+    align-items: center;
+    align-self: center;
+    animation: pulse 1.5s infinite;
+}
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,11 +2,13 @@
 import EventCard from '@/components/EventCard.vue';
 import { ref, onMounted, computed, watchEffect, defineProps } from 'vue';
 import EventService from '@/services/EventService';
+import SkeletonLoader from '@/components/SkeletonLoader.vue';
 
 const props = defineProps(['page']);
 
 const events = ref("");
 const totalEvents = ref(0);
+const loading = ref(true);
 
 const page = computed(() => {
   return props.page;
@@ -19,11 +21,13 @@ const hasNextPage = computed(() => {
 
 onMounted(() => {
   watchEffect(() => {
+    loading.value = true;
     events.value = null;
     EventService.getEvents(2, page.value)
       .then(response => {
         events.value = response.data;
         totalEvents.value = response.headers['x-total-count'];
+        loading.value = false; // set loading to false after data fetch
       })
       .catch(error => {
         console.log('There was an error:', error.response);
@@ -35,8 +39,10 @@ onMounted(() => {
 <template>
   <h1>The Good Events</h1>
   <div class="event-list">
-    <EventCard v-for="event in events" :key="event.id" :event='event' />
-
+    <SkeletonLoader v-if="loading" /> <!-- render the skeleton loader when loading is true -->
+    <EventCard v-for="event in events" :key="event.id" :event='event' v-else />
+    <!-- render the EventCard when loading is false -->
+    <div v-if="!loading && events.length === 0">No events found</div>
     <div class="pagination">
       <router-link id="page-prev" :to="{ name: 'EventList', query: { page: page - 1 } }" rel="prev"
         v-if="page != 1">&#60; Previous</router-link>
@@ -66,6 +72,9 @@ onMounted(() => {
   flex: 1;
   text-decoration: none;
   color: #2c3e50;
+  font-weight: bold;
+  font-family: monospace;
+  font-size: large;
 }
 
 #page-prev {
